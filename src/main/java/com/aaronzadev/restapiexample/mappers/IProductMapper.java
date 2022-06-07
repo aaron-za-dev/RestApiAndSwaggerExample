1e2ebf2380d85197199917e5f3e925beb3151790
Aarón Zúñiga <aaronzadev@gmail.com>
2022-06-07 00:28:24 -0500

    Change configurations for product mapper

--- src/main/java/com/aaronzadev/restapiexample/mappers/IProductMapper.java
+++ src/main/java/com/aaronzadev/restapiexample/mappers/IProductMapper.java
@@ -4,6 +4,7 @@ import com.aaronzadev.restapiexample.dto.product.ProductDto;
 import com.aaronzadev.restapiexample.persistence.entity.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,16 +12,17 @@ public interface IProductMapper {
 
     IProductMapper INSTANCE = Mappers.getMapper(IProductMapper.class);
 
-    /*@Mapping(source = "productId", target = "productId")
-    @Mapping(source = "productName", target = "productName")
-    @Mapping(source = "productDescription", target = "productDescription")
-    @Mapping(source = "productPrice", target = "productPrice")
-    @Mapping(source = "discontinued", target = "discontinued")*/
-    //@Mapping(source = "category.category", target = "productCategory")
+
+    @Mappings({
+       @Mapping(target = "productCategory", source = "category.categoryName")
+    })
     ProductDto productEntityToDto(Product product);
 
-    @Mapping(target = "productId", ignore = true)
-    //@Mapping(target = "discontinued", ignore = true)
+    @Mappings({
+       @Mapping(target = "productId", ignore = true),
+       @Mapping(target = "discontinued", ignore = true),
+       @Mapping(target = "category", ignore = true)
+    })
     Product productDtoToEntity(ProductDto productDto);
 
 
