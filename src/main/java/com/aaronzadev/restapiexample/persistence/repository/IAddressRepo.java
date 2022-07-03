8ca45c55ab6d379fe14d4a8a9f14516e350c0ea2
Aarón Zúñiga <aaronzadev@gmail.com>
2022-07-02 21:17:21 -0500

    .

--- src/main/java/com/aaronzadev/restapiexample/persistence/repository/IAddressRepo.java
+++ src/main/java/com/aaronzadev/restapiexample/persistence/repository/IAddressRepo.java
@@ -2,6 +2,8 @@ package com.aaronzadev.restapiexample.persistence.repository;
 
 import com.aaronzadev.restapiexample.persistence.entity.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IAddressRepo extends JpaRepository<AddressEntity, Long> {
 }
--- src/main/java/com/aaronzadev/restapiexample/persistence/repository/ICityRepo.java
+++ src/main/java/com/aaronzadev/restapiexample/persistence/repository/ICityRepo.java
@@ -4,7 +4,6 @@ import com.aaronzadev.restapiexample.persistence.entity.CityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface ICityRepo extends JpaRepository<CityEntity, Long> {
 }
