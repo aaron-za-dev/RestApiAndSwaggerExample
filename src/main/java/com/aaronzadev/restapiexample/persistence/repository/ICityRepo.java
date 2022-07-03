ad842e7ff4f7af4ab6e594c190fe60cfc13f4421
Aarón Zúñiga <aaronzadev@gmail.com>
2022-07-02 23:03:58 -0500

    .

--- src/main/java/com/aaronzadev/restapiexample/persistence/repository/ICityRepo.java
+++ src/main/java/com/aaronzadev/restapiexample/persistence/repository/ICityRepo.java
@@ -6,4 +6,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICityRepo extends JpaRepository<CityEntity, Long> {
+
+    //TODO avoid count query. This is an example
+    /*@Query(value = "SELECT city_id AS cityId, city AS cityName, country AS countryName, city.last_update AS lastUpdated FROM country INNER JOIN city USING (country_id)",
+           countQuery = "select count(city_id) from city", nativeQuery = true)
+    Page<TestDto> findAllWithCountry(Pageable pageable);*/
+
 }
